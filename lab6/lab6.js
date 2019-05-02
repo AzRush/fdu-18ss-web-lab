373c488f55df3c2a3cc896aa56ba81e9ff858b69
AzRush <574543413@qq.com>
2019-05-02 22:47:33 +0800

    Update lab6.js

--- lab6/lab6.js
+++ lab6/lab6.js
@@ -1,2 +1,29 @@
+var figure,figcaption,regularExpression;
+figure = document.querySelector("#featured img");
+figcaption  = document.querySelector("#featured figcaption");
+document.getElementById("thumbnails").addEventListener
+("click",
+    //changePath
+    function (e)
+    {
+                regularExpression = /[0-9]+\.jpg$/;
+                figure.src =  "images/medium/" + regularExpression.exec(e.target.src).toString();
+                figure.title = e.target.title;
+                figcaption.innerHTML  = e.target.title.toString();
+
+    }
+);
+    //changeStyle
+figcaption.style.transition = "opacity , 1s";
+document.getElementById("featured").onmouseover = function ()
+{
+    figcaption.style.opacity = 0.8;
+};
+document.getElementById("featured").onmouseout = function ()
+{
+            figcaption.style.opacity = 0;
+}
+
+
 
 
